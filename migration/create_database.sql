a172551518eff022693563c860cf93fa456bd625
gcharland1 <g.charland1@protonmail.com>
2024-05-09 20:02:53 -0400

    DB - Script migration creation DB

---
+++ migration/create_database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS activities (
+    id INTEGER NOT NULL PRIMARY KEY,
+    name TEXT,
+    days_in_a_row INTEGER
+);
+
+DELETE FROM activities WHERE id IS NOT NULL;
+
+INSERT INTO activities ('id', 'name', 'days_in_a_row') VALUES
+    (1, "Cube Rubik's", 0),
+    (2, "Course à pieds", 0),
+    (3, "Yoga", 0);
+
+COMMIT;
